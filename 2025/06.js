5392e0885632ad0a5e35841f643932c46d929033
Seokseong Jeon <sjeon87@gmail.com>
2025-12-06 15:30:28 +0900

    Solved: day 06 part 2

--- 2025/06.js
+++ 2025/06.js
@@ -2,6 +2,8 @@ import { readFileSync } from "node:fs";
 
 const content = readFileSync(process.argv[2], "utf8");
 const lines = content.split("\n");
+const numberLines = lines.slice(0, lines.length - 1);
+const operatorLine = lines[lines.length - 1];
 
 const part1 = () => {
 	const numberMatches = lines
@@ -31,6 +33,42 @@ const part1 = () => {
 
 part1();
 
-const part2 = () => {};
+const part2 = () => {
+	const subResults = [];
+	const numbers = [];
+	let lastOperator = "";
+
+	const processNumbers = (operator) => {
+		if (numbers.length) {
+			switch (lastOperator) {
+				case "+":
+					subResults.push(numbers.reduce((a, b) => a + b, 0));
+					break;
+				case "*":
+					subResults.push(numbers.reduce((a, b) => a * b, 1));
+					break;
+			}
+			numbers.length = 0;
+		}
+		lastOperator = operator;
+	};
+
+	for (let col = 0; col < operatorLine.length; col++) {
+		const operator = operatorLine[col];
+		if (operator === "+" || operator === "*") processNumbers(operator);
+		let number = 0;
+		for (let row = 0; row < numberLines.length; row++) {
+			const char = numberLines[row][col];
+			if (char === " ") continue;
+			number = number * 10 + Number(char);
+		}
+		if (!number) continue;
+		numbers.push(number);
+	}
+	processNumbers();
+
+	const total = subResults.reduce((a, b) => a + b, 0);
+	console.log(total);
+};
 
 part2();
